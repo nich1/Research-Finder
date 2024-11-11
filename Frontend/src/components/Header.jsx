c1993901761f2eea1cc273af653beb613f13d75d
Zachary48 <158526110+Zachary48@users.noreply.github.com>
2024-11-11 09:01:23 -0600

    style edits

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -6,13 +6,15 @@ import { Link } from 'react-router-dom';
 const Header = () => {
   return (
     <header className="centered-header">
-      <h1>Welcome to Research Finder</h1>
-      <nav>
-      <Link to="/" className="nav-link">Home</Link>
-        <Link to="/signin" className="nav-link">Sign In</Link>
-        <Link to="/register" className="nav-link">Register</Link>
-        <Link to="/post-research" className="nav-link">Post Research</Link> {/* New Link */}
-      </nav>
+      <div className="header-content">
+        <h1>Welcome to Research Finder</h1>
+        <nav>
+          <Link to="/" className="nav-link">Home</Link>
+          <Link to="/signin" className="nav-link">Sign In</Link>
+          <Link to="/register" className="nav-link">Register</Link>
+          <Link to="/post-research" className="nav-link">Post Research</Link>
+        </nav>
+      </div>
     </header>
   );
 };
