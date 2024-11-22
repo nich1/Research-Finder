f498132cbccf8c01873b31bdba6140768ec64c44
Zi Gorium <50188638+ZiGorium@users.noreply.github.com>
2024-11-22 16:27:20 -0600

    Update index.ts

--- index.ts
+++ index.ts
@@ -12,7 +12,7 @@ const port = process.env.PORT || 3000;
 
 // Enable CORS for all routes
 app.use(cors({
-  origin: ['https://research-finder-1000.web.app/', 'http://localhost:5173'], // Replace with your front-end's domain
+  origin: ['https://research-finder-1000.web.app', 'http://localhost:5173'], // Replace with your front-end's domain
   methods: 'GET,POST,PUT,DELETE', // Customize as needed
   credentials: true    // Enable this if you need to include cookies
 }));
